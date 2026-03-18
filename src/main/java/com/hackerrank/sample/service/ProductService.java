14db18567b287c1301bb21e500374de23d4c9ea6
Francisco Solé <franciscosoleescande@gmail.com>
2026-03-18 20:14:39 -0300

    add(ProductService): add interface methods

--- src/main/java/com/hackerrank/sample/service/ProductService.java
+++ src/main/java/com/hackerrank/sample/service/ProductService.java
@@ -1,4 +1,20 @@
 package com.hackerrank.sample.service;
 
+import com.hackerrank.sample.dto.ProductCreatedResponseDto;
+import com.hackerrank.sample.dto.ProductDetailsResponseDto;
+import com.hackerrank.sample.dto.ProductResponseDto;
+import com.hackerrank.sample.model.ProductInformation;
+import java.util.List;
+
 public interface ProductService {
+    ProductCreatedResponseDto createProduct(ProductInformation productInformation);
+
+    ProductDetailsResponseDto getProductById(Long id);
+    ProductResponseDto getProductByIds(List<Long> ids);
+    List<ProductResponseDto> getAllProducts();
+
+    void deleteProductById(Long id);
+    void deleteAllProducts();
+
+    void updateProduct(Long id, ProductInformation productInformation);
 }
