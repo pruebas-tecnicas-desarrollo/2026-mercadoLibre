6c612d79b578f177e66f7e6d67dc2c94a3568985
Francisco Solé <franciscosoleescande@gmail.com>
2026-03-19 00:11:49 -0300

    ref(ProductResponseDto): add @Builder

--- src/main/java/com/hackerrank/sample/dto/ProductResponseDto.java
+++ src/main/java/com/hackerrank/sample/dto/ProductResponseDto.java
@@ -1,9 +1,11 @@
 package com.hackerrank.sample.dto;
 
 import com.hackerrank.sample.model.ProductInformation;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class ProductResponseDto {
     private Long id;
     private ProductInformation information;
